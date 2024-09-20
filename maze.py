e811bee6837b1e094f17c8f2e8d555217e106bc3
Maurice Koreman <mg.koreman99@gmail.com>
2024-09-20 18:24:14 +0200

    create maze container with cells

--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
-from graphics import Cell
+from graphics import Cell, Point
+import time
 
 
 class Maze:
@@ -10,8 +11,9 @@ class Maze:
         self.cell_size_x = cell_size_x
         self.cell_size_y = cell_size_y
         self.win = win
-
         self._cells = []
+
+        self._create_cells()
 
     def _create_cells(self):
         # fills _cells with list of cells. Each top-level list is a column of Cell objects.
@@ -25,15 +27,30 @@ class Maze:
 
             self._cells.append(cell_row)
 
-        for row in self._cells:
-            for cell in row:
-                self._draw_cell()
+        for i, row in enumerate(self._cells):
+            for j, cell in enumerate(row):
+                self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
-        # calc x/y position of the cell based on i, j, cell size, and the x/y  position of the matrix itself
+        if self.win is None:
+            return
 
+        # calc x/y position of the cell based on i, j, cell size, and the x/y  position of the matrix itself
         # once calculated, draw the cell and call the maze's _animate() method
-        pass
+
+        # top-left corner
+        x1 = (j * self.cell_size_x) + self.x1
+        y1 = (i * self.cell_size_y) + self.y1
+
+        # bottom-right corner
+        x2 = ((j + 1) * self.cell_size_x) + self.x1
+        y2 = ((i + 1) * self.cell_size_y) + self.y1
+
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
 
     def _animate(self):
-        pass
+        if self.win is None:
+            return
+        self.win.redraw()
+        time.sleep(0.05)
